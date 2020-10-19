223325547eba777c8262cf944e4a560d487be6a1
Simon Alling <alling.simon@gmail.com>
2020-10-19 15:30:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Google: Hide "People also search for"
+
+Install it with your userscript manager of choice, for example [Violentmonkey](https://www.google.com/search?q=Violentmonkey).
---
+++ main.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Google: Hide "People also search for"
+// @description  Eliminates all those misclicks caused by the "People also search for" pop-ins.
+// @version      1.0.0
+// @include      /^https:\/\/(www\.)?google\.[a-z]+\/search/
+// @run-at       document-start
+// ==/UserScript==
+
+(() => {
+    "use strict";
+    const styleElement = document.createElement("style");
+    styleElement.textContent = `
+        .rc div[id^="eob_"] {
+            display: none !important;
+        }
+    `;
+    document.head.appendChild(styleElement);
+})();
